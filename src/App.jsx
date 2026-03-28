06b40f5a0a644005e9cdb10173510fd01c9d99fc
Dēivid Sans <david.mancillas@icloud.com>
2026-03-28 10:41:12 -0600

    add header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import "./blocks/app.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../blocks/header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Buscador todo-en-uno de Pokémon</h1>
+    </header>
+  );
+}
