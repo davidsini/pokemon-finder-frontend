67c518fcc7dc493ee5745da0fae64c7c19e32e51
Dēivid Sans <david.mancillas@icloud.com>
2026-03-28 11:03:07 -0600

    color

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import "./index.css";
 
-return (
-  <div>
-    <Header />
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
 
 export default App;
