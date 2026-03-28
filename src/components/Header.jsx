19e0103c576e8d1c0e0b160551b3fd1615c8b5cd
Dēivid Sans <david.mancillas@icloud.com>
2026-03-28 11:35:39 -0600

    add header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "../blocks/header.css";
+import logoDePokemon from "../images/pokemon-full-logo.png";
 
 export default function Header() {
   return (
     <header className="header">
+      <img src={logoDePokemon}></img>
       <h1>Buscador todo-en-uno de Pokémon</h1>
     </header>
   );
