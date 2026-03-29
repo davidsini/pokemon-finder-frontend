15fa90e9eefe8f4a010bf3cf66a2a1a202263cf9
Dēivid Sans <david.mancillas@icloud.com>
2026-03-29 13:31:07 -0600

    tailwind not working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "../blocks/header.css";
 import logoDePokemon from "../images/pokemon-full-logo.png";
 
 export default function Header() {
   return (
-    <header className="header">
+    <header className="flex w-screen bg-green-400 h-30">
       <img src={logoDePokemon}></img>
       <h1>Buscador todo-en-uno de Pokémon</h1>
     </header>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 5173, // Puerto fijo
+    strictPort: true,
+    watch: {
+      usePolling: true, // Esto obliga a Vite a revisar los archivos cada X tiempo
+    },
+  },
+  plugins: [react(), tailwindcss()],
+});
