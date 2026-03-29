eefdcf87ee31086f98fdb5ba487befa17a5e3cfa
Dēivid Sans <david.mancillas@icloud.com>
2026-03-29 10:40:16 -0600

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,12 @@ import logoDePokemon from "../images/pokemon-full-logo.png";
 
 export default function Footer() {
   return (
-    <Footer className="footer">
-      <img src={logoDePokemon}></img>
-      <h1>Pokémon Finder</h1>
-      <p>Dēivid Sans 2026. Todos los derechos reservados.</p>
-    </Footer>
+    <footer className="footer">
+      <div className="footer__logo-container">
+        <img src={logoDePokemon}></img>
+        <h2>Pokémon Finder</h2>
+      </div>
+      <p>© Dēivid Sans 2026. Todos los derechos reservados.</p>
+    </footer>
   );
 }
